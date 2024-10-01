4b08183b5bdb1053717381537d5c1fd29fd7c46e
Riccardo Maffeis <r.maffeis4@studenti.unibg.it>
2024-10-01 16:14:33 +0200

    Update App.java

--- Codice/hhh/src/main/java/ingsoftware/hhh/App.java
+++ Codice/hhh/src/main/java/ingsoftware/hhh/App.java
@@ -6,5 +6,6 @@ package ingsoftware.hhh;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
